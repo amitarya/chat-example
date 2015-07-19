1aa8cf1257817a1d4cbc02dd00a3f1f526bfccc6
Amit <arya_amit87@yahoo.com>
2015-07-19 11:09:33 -0700

    Adding: angular track by

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
+    console.log(msg);
     socket.broadcast.emit('chat message', msg);
   });
 });
